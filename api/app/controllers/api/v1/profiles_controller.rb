ca25a2e86b831a7734e8dffe1e95af96332c0399
franco83-2 <franco.84@gmail.com>
2017-10-05 13:39:28 -0700

    adjusted profile controllers to work w/ profile form

--- api/app/controllers/api/v1/profiles_controller.rb
+++ api/app/controllers/api/v1/profiles_controller.rb
@@ -1,16 +1,22 @@
 class Api::V1::ProfilesController < ApplicationController
   before_action :authenticate_user?
-  
+
     def index
       @profiles = Profile.all
       render json: @profiles, each_serializer: ProfileSerializer
     end
-  
+
     def show
-      @profile = Profile.find(params[:id])
-      render json: @profile, serializer: ProfileSerializer
+      if @profile = Profile.find(params[:id])
+        render json: @profile, serializer: ProfileSerializer
+      else
+        render json: {
+          error: "Profile Not Found",
+          status: 400
+        }, status: 400
+      end
     end
-  
+
     def create
       @user = get_current_user
       profile_save_params = profile_params
@@ -26,28 +32,29 @@ class Api::V1::ProfilesController < ApplicationController
         }, status: 400
       end
     end
-  
+
     def update
+      @profile = Profile.find(params[:id])
       if @profile.update(profile_params)
         render json: @profile, serializer: ProfileSerializer
       else
         render json: @profile.errors, status: 400
       end
     end
-  
+
     def destroy
+      @profile = Profile.find(params[:id])
       if @profile.destroy
         render json: { message: 'Profile was deleted successfully.' }
       else
         render json: @profile.errors, status: 400
       end
     end
-  
+
     private
-  
+
       def profile_params
         params.permit(:display_name, :first_name, :last_name)
       end
-  
+
   end
-  
